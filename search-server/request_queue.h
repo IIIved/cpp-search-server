0902dc594752d9691e473f0d22ed1a6cf5b2f31a
Oleg <FelexOTV@yandex.ru>
2022-04-11 14:36:13 +0300

    Splitting into files NEW

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -1,135 +1,41 @@
 #pragma once
 
-#include <algorithm>
-#include <atomic>
-#include <cmath>
-#include <execution>
-#include <iterator>
-#include <map>
-#include <set>
-#include <stdexcept>
+#include <cstdint>
+#include <deque>
 #include <string>
-#include <tuple>
 #include <vector>
 
 #include "document.h"
-#include "string_processing.h"
+#include "search_server.h"
 
-const int MAX_RESULT_DOCUMENT_COUNT = 5;
-
-using namespace std;
-
-class SearchServer {
+class RequestQueue {
 public:
-    template <typename StringContainer>
-    explicit SearchServer(const StringContainer& stop_words)
-        : stop_words_(MakeUniqueNonEmptyStrings(stop_words))  // Extract non-empty stop words
-    {
-        if (!all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
-            throw invalid_argument("Some of stop words are invalid"s);
-        }
-    }
-
-    explicit SearchServer(const string& stop_words_text);
-
-    void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings);
-
+    explicit RequestQueue(const SearchServer& search_server);
+ 
+    // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const {
-        const auto query = ParseQuery(raw_query);
-
-        auto matched_documents = FindAllDocuments(query, document_predicate);
-
-        sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
-            if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
-                return lhs.rating > rhs.rating;
-            } else {
-                return lhs.relevance > rhs.relevance;
-            }
-        });
-        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
-        }
-
-        return matched_documents;
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
+        return result;
     }
-
-    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const;
-
-    vector<Document> FindTopDocuments(const string& raw_query) const;
-
-    int GetDocumentCount() const;
-
-    int GetDocumentId(int index) const;
-
-    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const;
-
+ 
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
+ 
+    vector<Document> AddFindRequest(const string& raw_query);
+ 
+    int GetNoResultRequests() const;
+ 
 private:
-    
-    struct DocumentData {
-        int rating;
-        DocumentStatus status;
+    struct QueryResult {
+        uint64_t timestamp;
+        int results;
     };
-    const set<string> stop_words_;
-    map<string, map<int, double>> word_to_document_freqs_;
-    map<int, DocumentData> documents_;
-    vector<int> document_ids_;
-
-    bool IsStopWord(const string& word) const;
-
-    static bool IsValidWord(const string& word);
-   
-    vector<string> SplitIntoWordsNoStop(const string& text) const;
-
-    static int ComputeAverageRating(const vector<int>& ratings);
-
-    struct QueryWord {
-        string data;
-        bool is_minus;
-        bool is_stop;
-    };
-
-    QueryWord ParseQueryWord(const string& text) const;
-
-    struct Query {
-        set<string> plus_words;
-        set<string> minus_words;
-    };
-
-    Query ParseQuery(const string& text) const;
-
-    // Existence required
-    double ComputeWordInverseDocumentFreq(const string& word) const;
-
-    template <typename DocumentPredicate>
-    vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
-        map<int, double> document_to_relevance;
-        for (const string& word : query.plus_words) {
-            if (word_to_document_freqs_.count(word) == 0) {
-                continue;
-            }
-            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
-            for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-                const auto& document_data = documents_.at(document_id);
-                if (document_predicate(document_id, document_data.status, document_data.rating)) {
-                    document_to_relevance[document_id] += term_freq * inverse_document_freq;
-                }
-            }
-        }
-
-        for (const string& word : query.minus_words) {
-            if (word_to_document_freqs_.count(word) == 0) {
-                continue;
-            }
-            for (const auto [document_id, _] : word_to_document_freqs_.at(word)) {
-                document_to_relevance.erase(document_id);
-            }
-        }
-
-        vector<Document> matched_documents;
-        for (const auto [document_id, relevance] : document_to_relevance) {
-            matched_documents.push_back({document_id, relevance, documents_.at(document_id).rating});
-        }
-        return matched_documents;
-    }
+    deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
+    const static int min_in_day_ = 1440;
+ 
+    void AddRequest(int results_num) ;
 };
--- search-server/search_server.h
+++ search-server/search_server.h
@@ -12,53 +12,23 @@
 #include <tuple>
 #include <vector>
 
-
-
 #include "document.h"
 #include "string_processing.h"
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
-using namespace std;
-
-
 class SearchServer {
 public:
     template <typename StringContainer>
-    explicit SearchServer(const StringContainer& stop_words)
-        : stop_words_(MakeUniqueNonEmptyStrings(stop_words))  // Extract non-empty stop words
-    {
-        if (!all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
-            throw invalid_argument("Some of stop words are invalid"s);
-        }
-    }
+    explicit SearchServer(const StringContainer& stop_words);
 
     explicit SearchServer(const string& stop_words_text);
 
     void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings);
 
     template <typename DocumentPredicate>
-    vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const {
-        const auto query = ParseQuery(raw_query);
-
-        auto matched_documents = FindAllDocuments(query, document_predicate);
-
-        sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
-            if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
-                return lhs.rating > rhs.rating;
-            } else {
-                return lhs.relevance > rhs.relevance;
-            }
-        });
-        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
-        }
-
-        return matched_documents;
-    }
-
+    vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const ;
     vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const;
-
     vector<Document> FindTopDocuments(const string& raw_query) const;
 
     int GetDocumentCount() const;
@@ -69,12 +39,11 @@ public:
 
 private:
     
-   
-    
     struct DocumentData {
         int rating;
         DocumentStatus status;
     };
+    
     const set<string> stop_words_;
     map<string, map<int, double>> word_to_document_freqs_;
     map<int, DocumentData> documents_;
@@ -84,7 +53,6 @@ private:
 
     static bool IsValidWord(const string& word);
     
-
     vector<string> SplitIntoWordsNoStop(const string& text) const;
 
     static int ComputeAverageRating(const vector<int>& ratings);
@@ -108,7 +76,40 @@ private:
     double ComputeWordInverseDocumentFreq(const string& word) const;
 
     template <typename DocumentPredicate>
-    vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
+    vector<Document> FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const;
+};
+
+    template <typename StringContainer>
+    SearchServer::SearchServer(const StringContainer& stop_words)
+        : stop_words_(MakeUniqueNonEmptyStrings(stop_words))  // Extract non-empty stop words
+    {
+        if (!all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
+            throw invalid_argument("Some of stop words are invalid"s);
+        }
+    }
+
+    template <typename DocumentPredicate>
+    vector<Document> SearchServer::FindTopDocuments(const string& raw_query, DocumentPredicate      document_predicate) const {
+        const auto query = ParseQuery(raw_query);
+
+        auto matched_documents = FindAllDocuments(query, document_predicate);
+
+        sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
+            if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                return lhs.rating > rhs.rating;
+            } else {
+                return lhs.relevance > rhs.relevance;
+            }
+        });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+
+        return matched_documents;
+    }
+    
+    template <typename DocumentPredicate>
+    vector<Document> SearchServer::FindAllDocuments(const Query& query, DocumentPredicate document_predicate) const {
         map<int, double> document_to_relevance;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -138,4 +139,4 @@ private:
         }
         return matched_documents;
     }
-};
+    
